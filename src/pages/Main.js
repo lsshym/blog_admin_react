9f927858dbbe999b96962dd88c206a06483975a1
moyu <super_moyu@163.com>
2020-02-10 19:41:06 +0800

    更改为hash路由和添加homepage

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,14 +3,14 @@ import { Login } from './Login'
 import AdminIndex from './AdminIndex'
 import { ArticleList } from './ArticleList'
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, HashRouter } from 'react-router-dom'
 
 const Main = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Route path="/" exact component={Login}></Route>
             <Route path="/index/" component={AdminIndex}></Route>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
